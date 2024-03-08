c995e14326904d40d55450e5d54c00814e839034
sepehr <sepehrbehroozi@gmail.com>
2024-03-08 06:17:36 -0700

    I created the test that fails for has city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CustomListTest {
     private CustomList customList;
@@ -24,6 +26,18 @@ public class CustomListTest {
 
         // Assert
         assertEquals(1, customList.getCount());
+    }
+
+    @Test
+    public void testHasCity() {
+        // Arrange
+        City city1 = new City("City 1", "Province 1");
+        City city2 = new City("City 2", "Province 2");
+        customList.addCity(city1);
+
+        // Assert
+        assertTrue(customList.hasCity(city1));
+        assertFalse(customList.hasCity(city2)); // This assertion will fail initially
     }
 }
 
